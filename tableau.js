5089808b43204bd783d2ff9b690b62ad080e3966
kbarret <kevinbarret7@hotmail.fr>
2021-12-16 14:54:29 +0100

    réarrangement du code

--- tableau.js
+++ tableau.js
@@ -1,21 +1,17 @@
 import React from 'react';
-
-const tableau =(
- 
-                <table>
-                    <thead>
-                        <tr>
-                        <th>#</th>
-                        <th>First</th>
-                        <th>Last</th>
-                        <th>Status</th>
-                        </tr>
-                    </thead>
-                </table>
-             
+const tableau =
+(
+    <table>
+        <thead>
+            <tr>
+            <th>#</th>
+            <th>First</th>
+            <th>Last</th>
+            <th>Status</th>
+            </tr>
+        </thead>
+    </table>            
 )
-
-
 ReactDOM.render(
     tableau,
     document.getElementById('tableau')
