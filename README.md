076ed09325c1db99b1709ae2d7bb4cedb8895c29
kbarret <kevinbarret7@hotmail.fr>
2021-12-16 17:35:18 +0100

    react no fonct

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # React
+
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Ajout react en 1 Minute</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+  </head>
+  <body>
+
+    <h2>Tableau React</h2>
+    <div id="tableau"></div>
+    <p>rentrer des utilisateurs via une saisie </p>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          </tr>
+      </tbody>     
+    </table> 
+    <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+    <script src="tableau.js"></script><!-- Load our React component. -->
+
+  </body>
+</html>
+e('div',null,
+ ,
+
+                e('td',null,'kevin'),
+                e('td',null,'barret')),
+              e('tr',null,
+                e('th',{scope: 'row'},1),
+                e('td',null,'kevin'),
+                e('td',null,'barret'))   
+            ));
--- tableau.js
+++ tableau.js
@@ -1,18 +1,59 @@
-import React from 'react';
-const tableau =
-(
-    <table>
-        <thead>
-            <tr>
-            <th>#</th>
-            <th>First</th>
-            <th>Last</th>
-            <th>Status</th>
-            </tr>
-        </thead>
-    </table>            
-)
+const e = React.createElement;
+
+class Hello extends React.Component {
+  render() {
+    return e('div', null, `Bonjour `);
+  }
+}
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+    return e('button',{ onClick: () => this.setState({ liked: true }) },'Like');
+  }
+}
+const like = new Button;
+class Tableau extends React.Component {
+  render() {
+    return e('div',null,
+            e('table',{className: 'table'}, 
+              e('thead',null,
+                e('tr',null,
+                  e('th',{scope: 'col'},'id'),
+                  e('th',{scope: 'col'},'firstname'),
+                  e('th',{scope: 'col'},'lastname')
+                  )
+                ),
+            e('tbody',null,
+              e('tr',null,
+                e('th',{scope: 'row'},'1'),
+                e('td',null,'kevin'),
+                e('td',null,'barret'),
+                e('td',null,
+                  e('button',{className: 'btn btn-primary'},'like')
+                  )
+                ),
+              e('tr',null,
+                e('th',{scope: 'row'},'2'),
+                e('td',null,'Hugo'),
+                e('td',null,'react'),
+                e('td',null,
+                  like.render())
+                )
+              )   
+            )
+          );
+        
+   }
+}
+
 ReactDOM.render(
-    tableau,
-    document.getElementById('tableau')
-  );
+  e(Tableau, null),
+  document.getElementById('root')
+);
