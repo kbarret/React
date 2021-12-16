568875bb989ff3c8e70871bf78731936231222bc
Kevin B <94356251+kbarret@users.noreply.github.com>
2021-12-16 13:34:19 +0100

    Chargement projet react

---
+++ tableau.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const tableau =(
+ 
+                <table>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>First</th>
+                        <th>Last</th>
+                        <th>Status</th>
+                        </tr>
+                    </thead>
+                </table>
+             
+)
+
+
+ReactDOM.render(
+    tableau,
+    document.getElementById('tableau')
+  );
